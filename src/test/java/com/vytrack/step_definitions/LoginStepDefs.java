e76b0dd0d3747b7b9f472aca5a60eb5087ab4589
CybertekSchool <github@cybertekschool.com>
2020-10-20 12:54:54 -0400

    day2 cucumber

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -6,32 +6,37 @@ import io.cucumber.java.en.When;
 
 public class LoginStepDefs {
     @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() {
+    public void the_user_is_on_the_login_page() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("I open browser and navigate to vytrack login page");
     }
 
     @When("the user enters the driver information")
-    public void the_user_enters_the_driver_information() {
+    public void the_user_enters_the_driver_information() throws InterruptedException {
+        Thread.sleep(3000);
         System.out.println("I put user1 username and UserUser123 password and click login button");
 
     }
 
     @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() {
+    public void the_user_should_be_able_to_login() throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("I verify that title changed to Dashboard");
     }
 
     @When("the user enters the sales manager information")
-    public void the_user_enters_the_sales_manager_information() {
+    public void the_user_enters_the_sales_manager_information() throws InterruptedException {
+        Thread.sleep(3000);
         System.out.println("I enter sales manager username and password");
     }
-
     @When("the user enters the store manager information")
     public void the_user_enters_the_store_manager_information() {
-        //selenium code will come here
-        System.out.println("I enter store manager username and password");
-
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("I login an store manager");
     }
+
+
+
 
 
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,15 +1,16 @@
+@login
 Feature: Users should be able to login
-
+  @driver  @smoke
   Scenario: Login as a driver
     Given the user is on the login page
     When the user enters the driver information
     Then the user should be able to login
-
+  @sales_manager
   Scenario:  Login as a sales manager
     Given the user is on the login page
     When the user enters the sales manager information
     Then the user should be able to login
-
+  @store_manager @smoke
   Scenario: Login as a store manager
     Given the user is on the login page
     When the user enters the store manager information
