1ac98966d0c33f11cf61cc4b39874f6256ade90c
CybertekSchool <github@cybertekschool.com>
2020-10-22 13:33:20 -0400

    Day4: Contacts Updated

--- src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/ContactsStepDefs.java
@@ -1,22 +1,51 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.BasePage;
+import com.vytrack.pages.DashboardPage;
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
 public class ContactsStepDefs {
 
     @Given("the user logged in as {string}")
-    public void the_user_logged_in_as(String string) {
-        System.out.println("wait little bit");
+    public void the_user_logged_in_as(String userType) {
+        //go to login page
+        Driver.get().get(ConfigurationReader.get("url"));
+        //based on input enter that user information
+        String username =null;
+        String password =null;
+
+        if(userType.equals("driver")){
+            username = ConfigurationReader.get("driver_username");
+            password = ConfigurationReader.get("driver_password");
+        }else if(userType.equals("sales manager")){
+            username = ConfigurationReader.get("sales_manager_username");
+            password = ConfigurationReader.get("sales_manager_password");
+        }else if(userType.equals("store manager")){
+            username = ConfigurationReader.get("store_manager_username");
+            password = ConfigurationReader.get("store_manager_password");
+        }
+        //send username and password and login
+        new LoginPage().login(username,password);
     }
 
     @Then("the user should see following options")
     public void the_user_should_see_following_options(List<String> menuOptions) {
+        BrowserUtils.waitFor(2);
+        //get the list of webelement and convert them to list of string and assert
+        List<String> actualOptions = BrowserUtils.getElementsText(new DashboardPage().menuOptions);
 
-        System.out.println(menuOptions.size());
-        System.out.println(menuOptions);
+        Assert.assertEquals(menuOptions,actualOptions);
+        System.out.println("menuOptions = " + menuOptions);
+        System.out.println("actualOptions = " + actualOptions);
     }
 
 }
--- src/test/resources/features/Contacts.feature
+++ src/test/resources/features/Contacts.feature
@@ -12,12 +12,26 @@ Feature: Contacts page
     And the user enters the sales manager information
     When the user navigates to "Activities" "Calendar Events"
     Then the title contains "Calendar"
-  @wip
-  Scenario: Menu Options
+
+
+  Scenario: Menu Options Driver
     Given the user logged in as "driver"
     Then the user should see following options
       | Fleet      |
       | Customers  |
       | Activities |
       | System     |
+
+  @wip
+  Scenario: Menu Options Sales Manager
+    Given the user logged in as "sales manager"
+    Then the user should see following options
+      | Dashboards         |
+      | Fleet              |
+      | Customers          |
+      | Sales              |
+      | Activities         |
+      | Marketing          |
+      | Reports & Segments |
+      | System             |
 
